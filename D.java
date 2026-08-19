dcee8141ec35aaa20552d6ee59d9b612b5c07293
kuldipgulhane4 <kuldipgulhane4@gmail.com>
2026-08-19 14:17:23 +0530

    Update D.java
    pull update

--- D.java
+++ D.java
@@ -5,8 +5,9 @@
   
       <init> ()V Code LineNumberTable m1 main ([Ljava/lang/String;)V 
 SourceFile C.java 
-      ! method 1 is called  " # $ main method is start C   main method is end java/lang/Object java/lang/System out Ljava/io/PrintStream; java/io/PrintStream  println (Ljava/lang/String;)V !  
-       
+      ! method 1 is called        " # $ main method is start C   main method is end java/lang/Object java/lang/System out Ljava/io/PrintStream; java/io/PrintStream  println (Ljava/lang/String;)V !  
+ kk   
+   
    
         *· ±                
    %     	² ¶ ±       
